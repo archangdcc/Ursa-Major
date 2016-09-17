ea5f02d4600ee4fbebe051aa85d96e46d35241ae
Changchang Ding <dingchan@indiana.edu>
2016-09-16 22:13:04 -0400

    search: Add perft

--- search.py
+++ search.py
@@ -1,9 +1,23 @@
-import board
-import random
+# import random
+#
+# import board
 
 
 def perft(board, depth):
-    pass
+    if (board.last_move_won() or depth == 0):
+        return 1
+
+    moves = board.generate_moves()
+    if len(moves) == 0:
+        return 1
+
+    n = 0
+    for move in moves:
+        board.make_move(move)
+        n += perft(board, depth - 1)
+        board.unmake_last_move()
+
+    return n
 
 
 def find_win(board, depth):
