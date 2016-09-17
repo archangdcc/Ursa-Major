6d1c3e3aa0fe0a57bdc59886a49fad040449c24e
Changchang Ding <dingchan@indiana.edu>
2016-09-17 16:19:20 -0400

    search: Speed up perft

--- search.py
+++ search.py
@@ -1,15 +1,14 @@
-# import random
-#
-# import board
-
-
 def perft(board, depth):
-    if (board.last_move_won() or depth == 0):
+    if (board.last_move_won()):
         return 1
 
     moves = board.generate_moves()
+
     if len(moves) == 0:
         return 1
+
+    if (depth == 1):
+        return len(moves)
 
     n = 0
     for move in moves:
