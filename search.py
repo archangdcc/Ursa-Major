9515fac6ec7c018b1813f5215a20300ae3df844a
Changchang Ding <dingchan@indiana.edu>
2016-09-18 01:28:21 -0400

    search: alpha-beta pruning for finding force win

--- search.py
+++ search.py
@@ -19,5 +19,34 @@ def perft(board, depth):
     return n
 
 
+def find_win_ab(board, a, b, depth):
+    if board.last_move_won():
+        return -1
+    if depth == 0:
+        return 0
+    for move in board.generate_moves():
+        board.make_move(move)
+        v = - find_win_ab(board, - b, - a, depth - 1)
+        board.unmake_last_move()
+        if v >= b:
+            return b
+        if v > a:
+            a = v
+    return a
+
+
 def find_win(board, depth):
-    pass
+    no_result = False
+    for move in board.generate_moves():
+        board.make_move(move)
+        lose = find_win_ab(board, -2, 2, depth - 1)
+        board.unmake_last_move()
+        if lose == -1:
+            return "WIN BY PLAYING {}".format(move)
+        elif lose == 0:
+            no_result = True
+
+    if no_result:
+        return "NO FORCED WIN IN {} MOVES".format(depth)
+    else:
+        return "ALL MOVES LOSE"
