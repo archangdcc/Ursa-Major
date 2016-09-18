eaa2fd254cf472c15cbaa8e725db11d9ad456517
Changchang Ding <dingchan@indiana.edu>
2016-09-18 15:14:22 -0400

    search: store path in find_win

--- search.py
+++ search.py
@@ -19,37 +19,35 @@ def perft(board, depth):
     return n
 
 
-def find_win_ab(board, a, b, depth):
+def find_win_ab(board, a, b, depth, path):
     if board.last_move_won():
-        return -1
+        return -1, path
     if depth == 0:
-        return 0
+        return 0, path
     moves = board.generate_moves()
     if len(moves) == 0:
-        return 0
+        return 0, path
+
+    best_path = []
     for move in moves:
         board.make_move(move)
-        v = - find_win_ab(board, - b, - a, depth - 1)
+        v, sub_path = find_win_ab(board, - b, - a, depth - 1, [move])
+        v = - v
         board.unmake_last_move()
         if v >= b:
-            return b
+            return b, path
         if v > a:
+            best_path = sub_path
             a = v
-    return a
+    path.extend(best_path)
+    return a, path
 
 
 def find_win(board, depth):
-    no_result = False
-    for move in board.generate_moves():
-        board.make_move(move)
-        lose = find_win_ab(board, -2, 2, depth - 1)
-        board.unmake_last_move()
-        if lose == -1:
-            return "WIN BY PLAYING {}".format(move)
-        elif lose == 0:
-            no_result = True
-
-    if no_result:
-        return "NO FORCED WIN IN {} MOVES".format(depth)
-    else:
+    v, path = find_win_ab(board, -2, 2, depth, [])
+    if v == 1:
+        return "WIN BY PLAYING {}".format(path[0])
+    elif v == -1:
         return "ALL MOVES LOSE"
+    else:
+        return "NO FORCED WIN IN {} MOVES".format(depth)
