e02e6197dddbef61f223fa73e7858fca5e42f520
Changchang Ding <dingchan@indiana.edu>
2016-09-19 01:24:49 -0400

    board: simplify ref_table building process; store c4s in non-nested list; clarify naming

--- board.py
+++ board.py
@@ -1,39 +1,60 @@
-def _generate_win_positions():
+def _get_c4(name, start, direction, cid):
+    positions = [
+        (start[0] + i * direction[0], start[1] + i * direction[1])
+        for i in range(4)
+    ]
+
+    return {
+        'name': name,
+        'start': start,
+        'direction': direction,
+        'value': [0, 0],
+        'positions': positions,
+        'cid': cid
+    }
+
+
+def _generate_c4s():
     # All the possible connect-four's on the board
-    col_positions = [[{'start': (c, r), 'direction': (0, 1), 'value': [0, 0]}
-                      for r in range(3)] for c in range(7)]
-    row_positions = [[{'start': (c, r), 'direction': (1, 0), 'value': [0, 0]}
-                      for c in range(4)] for r in range(6)]
-    cmr_positions = [
-        [{'start': (0, 2), 'direction': (1, 1), 'value': [0, 0]}],
-        [{'start': (0 + i, 1 + i), 'direction': (1, 1), 'value': [0, 0]}
-         for i in range(2)],
-        [{'start': (0 + i, 0 + i), 'direction': (1, 1), 'value': [0, 0]}
-         for i in range(3)],
-        [{'start': (1 + i, 0 + i), 'direction': (1, 1), 'value': [0, 0]}
-         for i in range(3)],
-        [{'start': (2 + i, 0 + i), 'direction': (1, 1), 'value': [0, 0]}
-         for i in range(2)],
-        [{'start': (3, 0), 'direction': (1, 1), 'value': [0, 0]}],
-    ]
-    cpr_positions = [
-        [{'start': (3, 0), 'direction': (-1, 1), 'value': [0, 0]}],
-        [{'start': (4 - i, 0 + i), 'direction': (-1, 1), 'value': [0, 0]}
-         for i in range(2)],
-        [{'start': (5 - i, 0 + i), 'direction': (-1, 1), 'value': [0, 0]}
-         for i in range(3)],
-        [{'start': (6 - i, 0 + i), 'direction': (-1, 1), 'value': [0, 0]}
-         for i in range(3)],
-        [{'start': (6 - i, 1 + i), 'direction': (-1, 1), 'value': [0, 0]}
-         for i in range(2)],
-        [{'start': (6, 2), 'direction': (-1, 1), 'value': [0, 0]}],
-    ]
-    return [
-        col_positions,
-        row_positions,
-        cmr_positions,
-        cpr_positions,
-    ]
+    c4 = []
+
+    # Vertical
+    direction = (0, 1)
+    for c in range(7):
+        for r in range(3):
+            start = (c, r)
+            name = 'V{}{}'.format(c, r)
+            c4.append(_get_c4(name, start, direction, len(c4)))
+
+    # Horizontal
+    direction = (1, 0)
+    for r in range(6):
+        for c in range(4):
+            start = (c, r)
+            name = 'H{}{}'.format(r, c)
+            c4.append(_get_c4(name, start, direction, len(c4)))
+
+    # Diagonal
+    direction = (1, 1)
+    for j in range(3):
+        for i in range(j + 1):
+            name = 'D{}{}'.format(j, i)
+            c4.append(_get_c4(name, (i, 2 - j + i), direction, len(c4)))
+        for i in range(3 - j):
+            name = 'D{}{}'.format(j + 3, i)
+            c4.append(_get_c4(name, (j + i + 1, i), direction, len(c4)))
+
+    # Back-Diagonal
+    direction = (-1, 1)
+    for j in range(3):
+        for i in range(j + 1):
+            name = 'B{}{}'.format(j, i)
+            c4.append(_get_c4(name, (j - i + 3, i), direction, len(c4)))
+        for i in range(3 - j):
+            name = 'B{}{}'.format(j + 3, i)
+            c4.append(_get_c4(name, (6 - i, i + j), direction, len(c4)))
+
+    return c4
 
 
 def _pick(m, n, k=4):
@@ -47,42 +68,16 @@ class Board:
         self.table = [[] for i in range(7)]
         self.win = None
 
-        self.win_positions = _generate_win_positions()
+        self.c4s = _generate_c4s()
 
         # Use pre-build reference table to reduce calculation
-        self.ref_table = [[self._build_ref_table(c, r)
-                           for r in range(6)] for c in range(7)]
+        self.ref_table = [[[] for r in range(6)] for c in range(7)]
+        self._generate_ref_table()
 
-    def _build_ref_table(self, col, row):
-        # Return all the possible connect-four's that contain (col, row).
-        retvar = []
-        cpr = col - row
-        cmr = col + row
-
-        # vertical
-        for i in _pick(row, 6):
-            retvar.append(self.win_positions[0][col][i])
-
-        # horizontal
-        for i in _pick(col, 7):
-            retvar.append(self.win_positions[1][row][i])
-
-        # diagonal
-        if cpr >= -2 and cpr <= 0:
-            for i in _pick(col, 6 + cpr):
-                retvar.append(self.win_positions[2][cpr + 2][i])
-        elif cpr >= 1 and cpr <= 3:
-            for i in _pick(row, 7 - cpr):
-                retvar.append(self.win_positions[2][cpr + 2][i])
-
-        # back-diagonal
-        if cmr >= 3 and cmr <= 5:
-            for i in _pick(row, 1 + cmr):
-                retvar.append(self.win_positions[3][cmr - 3][i])
-        elif cmr >= 6 and cmr <= 8:
-            for i in _pick(6 - col, 12 - cmr):
-                retvar.append(self.win_positions[3][cmr - 3][i])
-        return retvar
+    def _generate_ref_table(self):
+        for c4 in self.c4s:
+            for pos in c4['positions']:
+                self.ref_table[pos[0]][pos[1]].append(c4)
 
     def generate_moves(self):
         return [i for i in range(7)
@@ -92,10 +87,10 @@ class Board:
         self.history.append(move)
         column = self.table[move]
 
-        for pos in self.ref_table[move][len(column)]:
-            pos['value'][self.next_player] += 1
-            if pos['value'][self.next_player] == 4:
-                self.win = pos
+        for c4 in self.ref_table[move][len(column)]:
+            c4['value'][self.next_player] += 1
+            if c4['value'][self.next_player] == 4:
+                self.win = c4
         column.append(self.next_player)
         self.next_player ^= 1
 
@@ -119,22 +114,20 @@ class Board:
     def __str__(self):
         # An illustration:
         # 0/1 for the first/second play's move.
-        # 'x' for empty tilde.
+        # 'x' for empty position.
 
         state = [['' for j in range(7)] for i in range(6)]
         for c, column in enumerate(self.table):
             r = 0
             while r < len(column):
-                state[5 - r][c] = str(column[c])
+                state[5 - r][c] = str(column[r])
                 r += 1
             while r <= 5:
                 state[5 - r][c] = 'x'
                 r += 1
 
         if self.win is not None:
-            for i in range(4):
-                c = self.win['start'][0] + i * self.win['direction'][0]
-                r = self.win['start'][1] + i * self.win['direction'][1]
+            for c, r in self.win['positions']:
                 # Highlight the win position using ANSI code.
                 state[5 - r][c] = '\x1b[1;31m{}\x1b[0m'.format(state[5 - r][c])
 
